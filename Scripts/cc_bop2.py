ca201fd91d32715ecec5218cae2955e68e711778
cc <57621900+ccteaher@users.noreply.github.com>
2024-08-07 16:33:20 +0800

    Update cc_bop2.py

--- Scripts/cc_bop2.py
+++ Scripts/cc_bop2.py
@@ -9,28 +9,19 @@ from mathutils import Vector, Matrix
 from math import acos
 import math
 
-"""
-    1~8个单个物体 随意放置
-    
-    *********切换不同的物体: P45 obj_ids=[1~21]
-                           P267 P475 更改输出文件夹名称
-    有干扰物体
-    
-"""
-# ######### 命令行的参数 ########################################################################
+
 parser = argparse.ArgumentParser()
 
-# 添加木桌+蓝色箱子模型对象，命名LBox_obj_model
 parser.add_argument('LBox_blend_model', nargs='?', default="hello/scene_model/wooden_table_02_1k.blend",
                     help="Path to the object file with the LBox object")
-# 添加输出路径对象，命名output_dir
+
 parser.add_argument('output_dir', nargs='?', default="hello/train_bop_output2",
                     help="Path to where the final files, will be saved")
-# 添加Bop物体
+
 parser.add_argument('bop_parent_path', help="Path to the bop datasets parent directory")
 args = parser.parse_args()
 
-# bproc初始化
+
 bproc.init()
 
 ##  Only Use GPU ####################################################################################
@@ -40,60 +31,59 @@ bpy.context.preferences.addons['cycles'].preferences.compute_device_type = 'CUDA
 bpy.context.preferences.addons["cycles"].preferences.get_devices()
 for d in bpy.context.preferences.addons["cycles"].preferences.devices:
     # only use GPU.
-    if d["name"] == 'Genuine Intel CPU 0000 @ 2.10GHz':
+    if d["name"] == '':
         d["use"] = 0
     else:
         d["use"] = 1
     print(d["name"], d["use"])
 ###############################################################################################
 
-# 定义随机数1~8
+
 rand_num = np.random.randint(1, 9)
 object_number = rand_num
-# 随机选择1~8个乐芙球
+
 
 target_bop_objs = []
-# 加载自己的六面BoP模型
+
 for i in range(object_number):
     target_bop_objs += bproc.loader.load_bop_PlyToObjs_single(bop_dataset_path=os.path.join(args.bop_parent_path, 'ourssix'),
                                                               obj_ids=[1], single_object=True, object_number=i)
 
-# 加载干扰物体
+
 # icbin_dist_bop_objs = bproc.loader.load_bop_objs(bop_dataset_path=os.path.join(args.bop_parent_path, 'icbin'), mm2m=True)
 # ycbv_dist_bop_objs = bproc.loader.load_bop_objs(bop_dataset_path=os.path.join(args.bop_parent_path, 'ycbv'), mm2m=True)
 # hb_dist_bop_objs = bproc.loader.load_bop_objs(bop_dataset_path=os.path.join(args.bop_parent_path, 'hb'), mm2m=True)
 
-# 获取对象所有的属性名
+
 # for i in range(object_number):
 #     print(target_bop_objs[i].__dict__)
 #     print(target_bop_objs[i].blender_obj)
-#     # 下面这句话相当于lefuqiu = bpy.data.objects['lefuqiu']
 #     lefuqiu=target_bop_objs[i].blender_obj
 #     print(lefuqiu.name)
 
-# 加载BOP数据集特性
+
 bproc.loader.load_bop_intrinsics(bop_dataset_path=os.path.join(args.bop_parent_path, 'ourssix'))
 
-# 设置阴影和隐藏对象
+
 # for obj in (target_bop_objs + icbin_dist_bop_objs + ycbv_dist_bop_objs + hb_dist_bop_objs):
 for obj in (target_bop_objs):
     obj.set_shading_mode('auto')
     obj.hide(True)
 
-# ##### 加载框子 ##########################################################################################
+# ##### load ##########################################################################################
 LBox_blend = bproc.loader.load_blend(args.LBox_blend_model)
 LBox = bproc.filter.one_by_attr(LBox_blend, "name", "Cube")
 
-# 从天花板上取样浅色和强度
+
 light_plane = bproc.object.create_primitive('PLANE', scale=[3, 3, 1], location=[0, 0, 10])
 light_plane.set_name('light_plane')
 light_plane_material = bproc.material.create('light_material')
 
-# 壳体上的采样点光
+
 light_point = bproc.types.Light()
 light_point.set_energy(200)
 
-# 采样两个光源
+
 light_plane_material.make_emissive(emission_strength=np.random.uniform(1, 6),
                                    emission_color=np.random.uniform([0.5, 0.5, 0.5, 1.0], [1.0, 1.0, 1.0, 1.0]))
 light_plane.replace_materials(light_plane_material)
@@ -102,7 +92,7 @@ location = bproc.sampler.shell(center=[0, 0, 3], radius_min=0.5, radius_max=1,
                                elevation_min=5, elevation_max=89)
 light_point.set_location(location)
 
-# ######### 设置灯光 ########################################################################
+# ######### set light ########################################################################
 
 area2 = bproc.types.Light()
 area2.set_type("AREA")
@@ -144,21 +134,21 @@ area6.set_energy(10)
 area6.set_color([1, 1, 1])
 area6.set_scale([5, 5, 5])
 
-# ######### 设置相机 ####################################################################################
-# 设置相机分辨率
+# ######### camera ####################################################################################
+
 image_width = 640
 image_height = 480
 # image_height = 640
 bproc.camera.set_resolution(image_width, image_height)
 
-# # 相机镜头方向固定-斜着
+
 # shuju = np.array([0.43, -0.22, 0.68, 0, 0, 0], dtype=np.double)
 
-# 相机镜头方向固定-正上方0.8m处
+
 shuju = np.array([0, 0, 0.8, 0, 0, 0], dtype=np.double)
 camera_position, camera_rotation = shuju[:3], shuju[3: 6]
 
-# 设置K矩阵，相机
+
 fx = 609.919868760916
 fy = 607.733032143467
 cx = 327.571937094492
@@ -170,36 +160,36 @@ K = np.array([
 ])
 bproc.camera.set_intrinsics_from_K_matrix(K, image_width, image_height)
 
-cam_poi = [0, 0, 0]  # 摄像机镜头目标
+cam_poi = [0, 0, 0]  
 rotation_matrixrotation_matrix = bproc.camera.rotation_from_forward_vec(cam_poi - camera_position)
 cam2world_matrix = bproc.math.build_transformation_mat(camera_position, rotation_matrixrotation_matrix)
 
 bproc.camera.add_camera_pose(cam2world_matrix)
-# bpy.data.objects['Camera'].select_set(1)  # 设置视野
+# bpy.data.objects['Camera'].select_set(1)  
 bpy.data.cameras['Camera'].angle = 1.23919
 
-# #### 设置世界颜色 ########################################################################################
+# #### world_color ########################################################################################
 bpy.data.worlds["World"].node_tree.nodes["Background"].inputs[0].default_value = (0.8, 0.749253, 0.749253, 1)
 
-# 获取HDR环境路径
+
 hdr_path = os.path.abspath(os.path.join(os.getcwd(), "hello", "scene_model", "hdr"))
 
 hdr_list = os.listdir(hdr_path)
-#获取HDR个数
+
 num_hdr_list = len(hdr_list)
-#设置随机HDR
+
 rand_seed = np.random.randint(0, num_hdr_list)
 real_hdr_path = os.path.join(hdr_path, hdr_list[rand_seed])
 
 bproc.world.set_world_background_hdr_img(real_hdr_path)
 
-# #### 位置[0,0,0]的姿态采样 ###############################################################################
+
 def sample_pose(obj: bproc.types.MeshObject):
     obj.set_location([0, 0, 0])
-    obj.set_rotation_euler(bproc.sampler.uniformSO3())  # 这可能只产生z轴旋转
+    obj.set_rotation_euler(bproc.sampler.uniformSO3())  
     obj.set_rotation_euler([0, 0, 0])
 
-# #### 空中姿态采样 ########################################################################################
+
 def sample_pose_sky(obj: bproc.types.MeshObject):
     # obj.set_location(np.random.uniform([-5, -5, 8], [5, 5, 12]))
     obj.set_location(np.random.uniform([-0.24, -0.2, 0.4], [0.24, 0.2, 1.8]))
@@ -213,12 +203,12 @@ def sample_pose_sky(obj: bproc.types.MeshObject):
 #         bproc.types.MeshObject(obj).set_cp("category_id", id)
 #         id += 1
 
-# 在场景中采样干扰物体 ##############################################################################################
+
 # sampled_distractor_bop_objs = list(np.random.choice(icbin_dist_bop_objs, size=1, replace=False))
 # sampled_distractor_bop_objs += list(np.random.choice(ycbv_dist_bop_objs, size=1, replace=False))
 # sampled_distractor_bop_objs += list(np.random.choice(hb_dist_bop_objs, size=1, replace=False))
 
-# 随机材料和设置物理
+
 # for obj in (target_bop_objs + sampled_distractor_bop_objs):
 for obj in (target_bop_objs):
     mat = obj.get_materials()[0]
@@ -231,20 +221,20 @@ for obj in (target_bop_objs):
     obj.hide(False)
 
 
-# #把所有的面移动到相机背面2m处 #############################################################################
+
 name = target_bop_objs[0].blender_obj.name
 for j in range(1, 4):
     for k in range(1, 3):
         bpy.data.objects[name + str(j) + "_" + str(k)].location[2] = 2
 
-# 让克隆的6面+1体：6个面跟随乐芙球盒子
+
 for m in range(object_number - 1):
     for j in range(1, 4):
         for k in range(1, 3):
             bpy.data.objects[name + str(j) + "_" + str(k) + ".00" + str(m + 1)].location[2] = 2
 
 
-# #### 添加着色器 ####################################################################################################
+
 mat = bpy.data.materials["Material.008"]
 nodes = mat.node_tree.nodes
 Material = nodes["Material Output"]
@@ -268,54 +258,54 @@ links.new(BsdfGlossy_node.outputs[0], MixShader_node.inputs[2])
 links.new(BsdfDiffuse_node.outputs[0], MixShader_node1.inputs[1])
 links.new(BsdfGlossy_node1.outputs[0], MixShader_node1.inputs[2])
 
-# #### 相机测试整体的乐芙球的姿态 ############################################################################
+
 bproc.object.sample_poses(
-    # objects_to_sample=target_bop_objs + sampled_distractor_bop_objs,  # 把物体列表放进去
-    objects_to_sample=target_bop_objs,  # 把物体列表放进去
-    sample_pose_func=sample_pose_sky,  # 调用空中姿态采样
+    # objects_to_sample=target_bop_objs + sampled_distractor_bop_objs, 
+    objects_to_sample=target_bop_objs,  
+    sample_pose_func=sample_pose_sky,  
     # objects_to_check_collisions=sampled_target_bop_objs,
 )
 
-# #### 设置篮子为刚体被动 ###################################################################################
+
 LBox.enable_rigidbody(active=False, collision_shape="MESH", friction=1, mass=1)
 
-# 设置整体乐芙球对象的刚体主动，指定所有物理属性（如质量、摩擦力等）
+
 for obj in target_bop_objs:
     obj.enable_rigidbody(active=True, collision_shape="BOX", friction=1, mass=1)
 
-# #### 做物理模拟 ##########################################################################################
+
 bproc.object.simulate_physics_and_fix_final_poses(min_simulation_time=4,
                                                   max_simulation_time=20,
                                                   check_object_interval=1)
 
-# 用于摄像机障碍物检查的BVH树
+
 # bop_bvh_tree = bproc.object.create_bvh_tree_multi_objects(target_bop_objs + sampled_distractor_bop_objs)
 bop_bvh_tree = bproc.object.create_bvh_tree_multi_objects(target_bop_objs)
 
 cam_poses = 0
 while cam_poses < 5:
-    # 采样位置
+
     location = bproc.sampler.shell(center=[0, 0, 0],
                                    radius_min=0.45,
                                    radius_max=1.08,
                                    elevation_min=50,
                                    elevation_max=90)
-    # 将场景中的兴趣点确定为最接近物体子集平均值的物体
+
     poi = bproc.object.compute_poi(np.random.choice(target_bop_objs, size=object_number, replace=False))
-    # 基于从位置到poi的矢量计算旋转
+
     # rotation_matrix = bproc.camera.rotation_from_forward_vec(poi - location,
     #                                                          inplane_rot=np.random.uniform(-3.14159, 3.14159))
     rotation_matrix = bproc.camera.rotation_from_forward_vec(poi - location)
-    # 添加基于位置和旋转的同源相机姿势
+ 
     cam2world_matrix = bproc.math.build_transformation_mat(location, rotation_matrix)
 
-    # 检查障碍物是否距离相机至少0.3米，并确保视野足够有趣
+
     if bproc.camera.perform_obstacle_in_view_check(cam2world_matrix, {"min": 0.3}, bop_bvh_tree):
-        # 坚持相机姿势
+
         bproc.camera.add_camera_pose(cam2world_matrix, frame=cam_poses)
         cam_poses += 1
 
-# #### 要生成bop数据集 必须给场景中的每个物体赋值属性category_id #############################################
+
 # id = 100
 # for obj in bpy.data.objects:
 #     if 'Cube' or 'wooden_table_02' in obj.name:
@@ -325,24 +315,24 @@ while cam_poses < 5:
 # bpy.data.objects["Cube"].hide_render = True
 
 
-# 激活深度渲染
+
 # bproc.renderer.enable_normals_output()
 bproc.renderer.enable_depth_output(activate_antialiasing=False)
 # bproc.renderer.enable_diffuse_color_output()
-# 渲染整个管道
+
 data0 = bproc.renderer.render()
 
-# 渲染以 BOP 格式包含深度图、位姿信息
+
 bproc.writer.write_bop(os.path.join(args.output_dir, 'test_bop_data'),
                        target_objects=target_bop_objs,
-                       dataset='ourssix',  # 保持此数据集的bop注释
+                       dataset='ourssix', 
                        depth_scale=0.05,
                        depths=data0["depth"],
                        colors=data0["colors"],
                        color_file_format="PNG",
                        append_to_existing_output=True)
 
-# #### 6个面跟随乐芙球盒子 ##################################################################################
+
 for i in range(3):
     for j in range(1, 4):
         for k in range(1, 3):
@@ -350,7 +340,7 @@ for i in range(3):
             bpy.data.objects[name + str(j) + "_" + str(k)].rotation_euler[i] = \
             bpy.data.objects[name].rotation_euler[i]
 
-# 让克隆的6面+1体：6个面跟随乐芙球盒子
+
 for m in range(object_number - 1):
     for i in range(3):
         for j in range(1, 4):
@@ -359,26 +349,26 @@ for m in range(object_number - 1):
                 bpy.data.objects[name + ".00" + str(m + 1)].location[i]
                 bpy.data.objects[name + str(j) + "_" + str(k) + ".00" + str(m + 1)].rotation_euler[i] = \
                 bpy.data.objects[name + ".00" + str(m + 1)].rotation_euler[i]
-# #### 删除乐芙球整体 #####################################################################################
+
 # for i in range(object_number):
 #     bpy.context.scene.collection.objects.unlink(target_bop_objs[i].blender_obj)
 
 for obj in (target_bop_objs):
     obj.disable_rigidbody()
     obj.hide(True)
-# #### 计算物体面和相机的角度 #######################################################################################
-# 相机轴的方向
+
+
 camera = bpy.context.scene.camera
-# 只计算旋转矩阵
+
 euler = camera.matrix_world.to_euler('XYZ')
 mat1 = Matrix.Rotation(euler.x, 4, 'X')
 mat2 = Matrix.Rotation(euler.y, 4, 'Y')
 mat3 = Matrix.Rotation(euler.z, 4, 'Z')
 mat = mat1 @ mat2 @ mat3
-# 相机法线方向
+
 v1 = mat @ Vector((0, 0, -1))
 
-# ### 获取所有的6个面 ############################################################################################
+# ### six face ############################################################################################
 obj = []
 obj1 = []
 obj2 = []
@@ -388,7 +378,7 @@ obj5 = []
 obj6 = []
 obj7 = []
 
-# 获取所有的6个面
+
 for m in range(1, object_number+1):
     for j in range(1, 4):
         for k in range(1, 3):
@@ -410,12 +400,10 @@ for m in range(1, object_number+1):
                 obj7.append(bpy.data.objects[name + str(j) + "_" + str(k) + ".00" + str(m - 1)])
 
 
-# ### 设置实例分割每一个面的Mask编号 #################################################################################
-# 不想显示的面标记设为0
 k = 1
-max1 = 60   # 最大角70度
-max2 = 45   # 最大角30度
-max3 = 30   # 最大角30度
+max1 = 60   
+max2 = 45   
+max3 = 30   
 
 OBJ = []
 for m in range(1, object_number+1):
@@ -510,18 +498,18 @@ for m in range(1, object_number+1):
             ob = bpy.data.objects[NAME5]
 
         bpy.context.view_layer.update()
-        # 指定平面法线的方向（局部坐标系X和Z轴被反了，所以需要调换）
+
         euler = ob.matrix_world.to_euler('XYZ')
         mat1 = Matrix.Rotation(euler.z, 4, 'Z')
         mat2 = Matrix.Rotation(euler.y, 4, 'Y')
         mat3 = Matrix.Rotation(euler.x, 4, 'X')
         mat = mat1 @ mat2 @ mat3
         norm = mat @ ob.data.polygons[0].normal
-        # 算夹角
+
         radian = acos(v1 @ norm)
         angle = 180 - math.degrees(radian)
         # print(angle)
-        # 角度不适合抓取的直接从实例分割中抹除
+
         if (j == 0 or j == 1) and (angle > max1):
             bproc.types.MeshObject(OBJ[j]).set_cp("category_id", 0)
         elif (j == 2 or j == 3) and (angle > max2):
@@ -530,17 +518,17 @@ for m in range(1, object_number+1):
             bproc.types.MeshObject(OBJ[j]).set_cp("category_id", 0)
 
 
-# # #### 对相机视角能看到的面分割（已经排除了角度过大的面） ########################################
+
 seg_data = bproc.renderer.render_segmap(map_by=["instance", "class", "name"])
 # print(seg_data['instance_attribute_maps'][0])
 
-# # #### 渲染 #################################################################################
-# 激活正常渲染
+
+
 bproc.renderer.enable_normals_output()
-# 渲染整个管道
+
 data = bproc.renderer.render()
 
-# 渲染以 COCO 格式包含实例分割,polygon解决mask形变问题
+
 bproc.writer.write_coco_annotations(os.path.join(args.output_dir, 'test_coco_data'),
                         instance_segmaps=seg_data["instance_segmaps"],
                         instance_attribute_maps=seg_data["instance_attribute_maps"],
